1b2e95138747deb9b126cf006fe9d9487ceefc82
mr-steveb <55226660+mr-steveb@users.noreply.github.com>
2021-05-06 19:43:42 +1000

    playing with go mod tidy

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func main() {
-	println("Hello World")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+
 }
